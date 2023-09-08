8d3b8eb21d0842197f7b45cd5fcea2192373be68
Haianh4705 <haianhnguyen472005@gmail.com>
2023-09-08 20:21:21 +0700

    bt1

---
+++ c-aptech/B1/bt1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    printf("Name: Hai Anh\n");
+    printf("Age: 18\n");
+    printf("Address: Thanh Liet, Thanh Tri, Ha Noi\n");
+    printf("Email: haianhnguyen472005@gmail.com\n");
+    printf("Phone Number: 0836526***\n");
+    return 0;
+}
